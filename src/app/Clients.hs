e46a2ec1e44660ff1e80f2ae6acbb602b1dca589
jtamu <gtamu.dip@gmail.com>
2025-04-12 13:43:07 +0900

    feat: モデル名の取りうる値を制限

--- src/app/Clients.hs
+++ src/app/Clients.hs
@@ -8,7 +8,16 @@ import Requests (Prompt, ReqBody (ReqBody))
 import Responses (ResBody (choices), ResMessage (ResMessage), ResMessageContent (content))
 import System.Environment (getEnv)
 
-newtype ChatOpenAI = ChatOpenAI {model_name :: String} deriving (Show)
+data OpenAIModelName = GPT4O | GPT3_5Turbo
+
+instance Show OpenAIModelName where
+  show GPT4O = "gpt-4o"
+  show GPT3_5Turbo = "gpt-3.5-turbo"
+
+newtype ChatOpenAI = ChatOpenAI {modelName :: OpenAIModelName} deriving (Show)
+
+openAIModelNameStr :: ChatOpenAI -> String
+openAIModelNameStr (ChatOpenAI modelName) = show modelName
 
 strOutput :: ResBody -> Maybe String
 strOutput resBody = case choices resBody of
@@ -18,7 +27,7 @@ strOutput resBody = case choices resBody of
 invoke :: ChatOpenAI -> Prompt -> IO ResBody
 invoke model prompt = do
   openaiApiKey <- getEnv "OPENAI_API_KEY"
-  let reqbody = ReqBody (model_name model) prompt
+  let reqbody = ReqBody (openAIModelNameStr model) prompt
   let req = setRequestHeaders [(hAuthorization, BS.pack $ "Bearer " ++ openaiApiKey), (hContentType, BS.pack "application/json")] $ setRequestBodyJSON reqbody $ parseRequest_ "POST https://api.openai.com/v1/chat/completions"
   res <- httpJSON req
   return (getResponseBody res :: ResBody)
--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
-import Clients (ChatOpenAI (ChatOpenAI), invoke, strOutput)
+import Clients (ChatOpenAI (ChatOpenAI), OpenAIModelName (GPT4O), invoke, strOutput)
 import Requests (ReqMessage (ReqMessage))
 
 main :: IO ()
 main = do
   let prompt = [ReqMessage "system" "語尾にニャーとつけてください", ReqMessage "user" "猫は好きですか？"]
-  let model = ChatOpenAI "gpt-4o"
+  let model = ChatOpenAI GPT4O
   res <- invoke model prompt
   let out = strOutput res
   case out of
